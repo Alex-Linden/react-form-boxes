ca420c233e47f061e6468539da8f35718aecef57
Alex Linden <alextl2004@gmail.com>
2023-07-19 15:28:09 -0700

    fix errors

--- src/Box.js
+++ src/Box.js
@@ -1,24 +1,25 @@
 import React from "react";
 
-/** 
+/**
  * props:
  * - height
  * - width
  * - backgroundColor
  * - removeBox (callback function)
-TODO: This component should display a div with a background color, width, and height 
-based on the props passed to it. 
+TODO: This component should display a div with a background color, width, and height
+based on the props passed to it.
 
 */
 
-function Box({ height, width, backgroundColor, removeBox }){ 
-  
-  return(
+function Box({ height, width, backgroundColor, removeBox }) {
+
+  return (
     <div className="Box">
-      <div style={{ 
-        height: height + "px", 
-        width: width + "px", 
-        backgroundColor }}></div>
+      <div style={{
+        height: height + "px",
+        width: width + "px",
+        backgroundColor
+      }}></div>
       <button onClick={removeBox}>X</button>
     </div>
   );
